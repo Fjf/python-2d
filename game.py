914fd11ed7c24456ec9019566a83f47697acd538
Meepp <m.mourcous@hotmail.com>
2018-04-12 15:01:03 +0200

    Player object

--- game.py
+++ game.py
@@ -1,5 +1,43 @@
 # import the pygame module, so you can use it
 import pygame
+import time
+
+WHITE = (255, 255, 255)
+
+class Player(object):
+    def __init__(self, screen):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([30, 30])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Draw the car (a rectangle!)
+        pygame.draw.rect(self.image, pygame.Color(255, 0, 0, 128), [0, 0, 30, 30])
+
+        # Instead we could load a proper pciture of a car...
+        self.image = pygame.image.load("LUL.png").convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.screen = screen
+
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+        dist = 1
+        if key[pygame.K_LEFT]:
+           self.rect.move_ip(-1, 0)
+        if key[pygame.K_RIGHT]:
+           self.rect.move_ip(1, 0)
+        if key[pygame.K_UP]:
+           self.rect.move_ip(0, -1)
+        if key[pygame.K_DOWN]:
+           self.rect.move_ip(0, 1)
+
+    def draw(self, surface):
+        pygame.draw.rect(self.screen, pygame.Color(255, 0, 0, 128), self.rect)
 
 # define a main function
 def main():
@@ -17,6 +55,8 @@ def main():
     # define a variable to control the main loop
     running = True
 
+    player = Player(screen)
+
     # main loop
     while running:
         # event handling, gets all event from the eventqueue
@@ -25,15 +65,13 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-<<<<<<< HEAD
-        pygame.draw.rect(screen, RED, [75, 10, 50, 20], 2)
-        pygame.display.flip()
-=======
 
-        pygame.draw.rect(screen, pygame.Color(255, 0, 0, 128), pygame.Rect(0, 0, 20, 20))
-        pygame.display.flip()
 
->>>>>>> 2691a2ceefe67532ff9671ea84f1708cc079b489
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+
+
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
