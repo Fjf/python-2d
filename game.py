cc26dab55b01bf0b59e6ee4d186e4c244ef9e5b7
Meepp <m.mourcous@hotmail.com>
2018-04-13 15:35:35 +0200

    small stuff

--- game.py
+++ game.py
@@ -205,6 +205,6 @@ def main():
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
-if __name__=="__main__":
+if __name__ == "__main__":
     # call the main function
     main()
